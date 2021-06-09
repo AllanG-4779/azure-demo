d31ac65b0531fc26534e8890fc23304eb5640636
Allang4779 <allang4779@gmail.com>
2021-06-09 17:10:01 +0300

    deleted experimental views

--- app.js
+++ app.js
@@ -8,12 +8,7 @@ app.set("view engine", "pug")
 app.use(express.static("public"))
 //routes
 
-app.get("/", (req,res)=>{
-    res.render("body",{title:"Pug trial", message:"Hallo Welcome to nodejs"})
-})
-app.get("/home",(req,res)=>{
-    res.render("hommie")
-})
+
 
 const PORT  = process.env.PORT || 5000
 
